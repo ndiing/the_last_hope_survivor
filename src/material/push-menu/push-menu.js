cc8c218d718e574c7ed8ac3586c7c63006069215
Ridho Prasetya <ndiing.inc@gmail.com>
2026-04-01 09:59:29 +0700

    chore(update)

--- src/material/push-menu/push-menu.js
+++ src/material/push-menu/push-menu.js
@@ -73,7 +73,7 @@ class MDPushMenu extends MDComponent {
                 <md-push-menu-item
                     .parent="${parent}"
                     .leading="${ifDefined(parent.leading)}"
-                    .label="${'Back'}"
+                    .label="${ifDefined(parent.label)}"
                     .supportingText="${ifDefined(parent.supportingText)}"
                     .trailing="${ifDefined(parent.trailing)}"
                     .selected="${this.selected.has(parent.id)}"
