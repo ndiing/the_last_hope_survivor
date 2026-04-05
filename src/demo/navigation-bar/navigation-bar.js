08cb162a21fe6358fd833170bdb50727db55d94c
Ridho Prasetya <ndiing.inc@gmail.com>
2026-04-06 00:15:55 +0700

    fix(component): fixed navigation rail behavior

--- src/demo/navigation-bar/navigation-bar.js
+++ src/demo/navigation-bar/navigation-bar.js
@@ -9,18 +9,18 @@ class DemoNavigationBar extends MDComponent {
 
                 <md-layout-column-item expanded="6" medium="8" compact="4">
                     <md-navigation-bar .items="${[
-                        {id:'0',leading:[{component:'icon',icon:'image'}],label:'Item 1',selected:true},
-                        {id:'1',leading:[{component:'icon',icon:'image'}],label:'Item 2'},
-                        {id:'2',leading:[{component:'icon',icon:'image'}],label:'Item 3'},
-                        {id:'3',leading:[{component:'icon',icon:'image'}],label:'Item 4'},
+                        {id:'0',leading:[{component:'icon',icon:'image'}],label:'Lorem',selected:true},
+                        {id:'1',leading:[{component:'icon',icon:'image'}],label:'ipsum'},
+                        {id:'2',leading:[{component:'icon',icon:'image'}],label:'dolor'},
+                        {id:'3',leading:[{component:'icon',icon:'image'}],label:'sit'},
                     ]}" layout="vertical"></md-navigation-bar>
                 </md-layout-column-item>
                 <md-layout-column-item expanded="12" medium="8" compact="4">
                     <md-navigation-bar .items="${[
-                        {id:'4',leading:[{component:'icon',icon:'image'}],label:'Item 1',selected:true},
-                        {id:'5',leading:[{component:'icon',icon:'image'}],label:'Item 2'},
-                        {id:'6',leading:[{component:'icon',icon:'image'}],label:'Item 3'},
-                        {id:'7',leading:[{component:'icon',icon:'image'}],label:'Item 4'},
+                        {id:'4',leading:[{component:'icon',icon:'image'}],label:'Lorem',selected:true},
+                        {id:'5',leading:[{component:'icon',icon:'image'}],label:'ipsum'},
+                        {id:'6',leading:[{component:'icon',icon:'image'}],label:'dolor'},
+                        {id:'7',leading:[{component:'icon',icon:'image'}],label:'sit'},
                     ]}" layout="horizontal"></md-navigation-bar>
                 </md-layout-column-item>
 
--- src/demo/navigation-rail/navigation-rail.js
+++ src/demo/navigation-rail/navigation-rail.js
@@ -9,18 +9,18 @@ class DemoNavigationRail extends MDComponent {
 
                 <md-layout-column-item expanded="6" medium="8" compact="4">
                     <md-navigation-rail style="height:480px;" .items="${[
-                        {id:'0',leading:[{component:'icon',icon:'image'}],label:'Item 1',selected:true},
-                        {id:'1',leading:[{component:'icon',icon:'image'}],label:'Item 2',trailing:[{component:'badge',label:0}]},
-                        {id:'2',leading:[{component:'icon',icon:'image'}],label:'Item 3',trailing:[{component:'badge',label:3}]},
-                        {id:'3',leading:[{component:'icon',icon:'image'}],label:'Item 4',trailing:[{component:'badge',label:123}]},
+                        {id:'0',leading:[{component:'icon',icon:'image'}],label:'Lorem',selected:true},
+                        {id:'1',leading:[{component:'icon',icon:'image'}],label:'ipsum',trailing:[{component:'badge',label:0}]},
+                        {id:'2',leading:[{component:'icon',icon:'image'}],label:'dolor',trailing:[{component:'badge',label:3}]},
+                        {id:'3',leading:[{component:'icon',icon:'image'}],label:'sit',trailing:[{component:'badge',label:123}]},
                     ]}"></md-navigation-rail>
                 </md-layout-column-item>
                 <md-layout-column-item expanded="6" medium="8" compact="4">
                     <md-navigation-rail style="height:480px;" .items="${[
-                        {id:'4',leading:[{component:'icon',icon:'image'}],label:'Item 1',selected:true},
-                        {id:'5',leading:[{component:'icon',icon:'image'}],label:'Item 2',trailing:[{component:'badge',label:0}]},
-                        {id:'6',leading:[{component:'icon',icon:'image'}],label:'Item 3',trailing:[{component:'badge',label:3}]},
-                        {id:'7',leading:[{component:'icon',icon:'image'}],label:'Item 4',trailing:[{component:'badge',label:123}]},
+                        {id:'4',leading:[{component:'icon',icon:'image'}],label:'Lorem',selected:true},
+                        {id:'5',leading:[{component:'icon',icon:'image'}],label:'ipsum',trailing:[{component:'badge',label:0}]},
+                        {id:'6',leading:[{component:'icon',icon:'image'}],label:'dolor',trailing:[{component:'badge',label:3}]},
+                        {id:'7',leading:[{component:'icon',icon:'image'}],label:'sit',trailing:[{component:'badge',label:123}]},
                     ]}" expanded></md-navigation-rail>
                 </md-layout-column-item>
 
